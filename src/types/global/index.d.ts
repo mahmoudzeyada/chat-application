f814961cf87003c68da2ed178d0478c2cf23d2cc
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-11-12 19:05:19 +0200

    reconstructing the project

--- src/types/global/index.d.ts
+++ src/types/global/index.d.ts
@@ -2,3 +2,12 @@ declare interface IMessage {
   text: string;
   createdAt: string;
 }
+
+declare interface IElement extends HTMLFormControlsCollection {
+  message: HTMLInputElement;
+}
+
+declare interface Window {
+  jQuery: any;
+  $: any;
+}
--- src/utils/message.ts
+++ src/utils/message.ts
@@ -1,9 +1,12 @@
 import { IMessage } from "../interfaces/message.interface";
 import * as moment from "moment";
+import { ICoords } from "../interfaces/chat.interface";
 
 export const timeStampMessage = (text: string): IMessage => {
   const now = new Date().getTime();
-  const createdAt = moment(now).format("h:mm");
+  const timeInNumbers = moment(now).format("h:mm a");
+  const timeFromNow = moment(now).fromNow();
+  const createdAt = `${timeInNumbers} | ${timeFromNow}`;
   return {
     text,
     createdAt
